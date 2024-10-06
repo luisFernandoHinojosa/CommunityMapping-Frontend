4b073529cbace8e179015be33dfe4ed6cb482e11
luis <luis111ferrivera@gmail.com>
2024-10-06 15:34:40 -0400

    new-graficas

--- src/components/GraficaTorta.jsx
+++ src/components/GraficaTorta.jsx
@@ -5,33 +5,32 @@ import { PROBLEMATIC_DATA } from '../utils/constantes';
 const DATA_GRAFICA = PROBLEMATIC_DATA.visual[0].data.problematic;
 // 4330
 export const GraficaTorta = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    console.log(DATA_GRAFICA)
-    const onPieEnter = (_, index) => {
-      setActiveIndex(index);  
-    };
+  const [activeIndex, setActiveIndex] = useState(0);
+  console.log(DATA_GRAFICA)
+  const onPieEnter = (_, index) => {
+    setActiveIndex(index);
+  };
 
-    return (
-      <ResponsiveContainer width="100%" height={700}>
-        <PieChart width={400} height={400}>
-          <Pie
-            activeIndex={activeIndex}
-            activeShape={renderActiveShape}
-            data={DATA_GRAFICA}  // Usamos los nuevos datos
-            cx="50%"
-            cy="55%"
-            innerRadius={180}
-            outerRadius={250}
-            fill="#8884d8"
-            dataKey="quantity"  // Indicamos que el valor es 'cantidad'
-            onMouseEnter={onPieEnter}
-          />
-        </PieChart>
-      </ResponsiveContainer>
-    );
+  return (
+    <ResponsiveContainer width="100%" height={700}>
+      <PieChart width={400} height={400}>
+        <Pie
+          activeIndex={activeIndex}
+          activeShape={renderActiveShape}
+          data={DATA_GRAFICA}
+          cx="50%"
+          cy="55%"
+          innerRadius={180}
+          outerRadius={250}
+          fill="#8884d8"
+          dataKey="quantity"
+          onMouseEnter={onPieEnter}
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  );
 };
 
-// Sector activo actualizado para mostrar 'mes', 'municipio', y 'cantidad'
 const renderActiveShape = (props) => {
   const RADIAN = Math.PI / 180;
   const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
@@ -70,9 +69,9 @@ const renderActiveShape = (props) => {
       />
       <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
       <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`quantity: ${value}`}</text> {/* Mostramos la cantidad */}
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`quantity: ${value}`}</text>
       <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-        {`(locality: ${payload.locality})`} {/* Mostramos el municipio */}
+        {`(locality: ${payload.locality})`}
       </text>
       <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={36} textAnchor={textAnchor} fill="#999">
         {`(Rate ${(percent * 100).toFixed(2)}%)`}
